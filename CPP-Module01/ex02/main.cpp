1b5f16cada2d716ad2e247ecc2e7c3240b009667
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2022-12-01 22:24:11 +0300

    easy

---
+++ CPP-Module01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tandroma <tandroma@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 23:19:03 by tandroma          #+#    #+#             */
+/*   Updated: 2022/10/19 10:04:34 by tandroma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	string;
+	std::string	*stringPTR;
+
+	string = "HI THIS IS BRAIN";
+    /* A pointer to the string */
+	stringPTR = &string;
+    /* A reference to the string */
+	std::string	&stringREF = string;
+	std::cout << "The memory address of the string variable: " << &string << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+    std::cout << "The value of the string variable: " << string << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+}
